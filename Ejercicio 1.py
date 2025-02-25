5e5ea9f9f7560a20957ab853f23b16daf9243cf7
UP240479 <up240479@alumnos.upa.edu.mx>
2025-02-25 08:39:41 -0600

    soy pro

---
+++ Ejercicio 1.py
@@ -0,0 +1,34 @@
+#Día 3
+#Área triángulo 
+base = float(input("ingresa el valor de la base:"))
+altura = float(input("ingrese el valor de la altura:"))
+area = 0.5 * base * altura
+print("El area del triangulo es:",area)
+
+#Perimetro de un triangulo
+lado = float(input("Ingrese el primer valor:"))
+lado2 = float(input("Ingrese el segundo valor:"))
+lado3 = float(input("Ingrese el tercer valor"))
+per = lado + lado2 + lado3 
+print("El perimetro del triangulo es:", per)
+
+#Calculos de un rectangulo
+largo = float(input("Ingresa el largo del rectangulo:"))
+ancho = float(input("Ingrese el ancho del rectangulo:"))
+area = largo * ancho
+per = 2*(largo + ancho) 
+print("El perimetro de tu rectangulo es:", per)
+print("El area de tu rectangulo es:", area)
+
+#Area de un circulo 
+pi = 3.1416
+radio = float(input("Ingresa el radio del circulo:"))
+area = pi * radio **2
+print("El area de tu circulo es:", area)
+circunferencia = 2 * pi * r
+print("La circunferencia de tu circulo es:", circunferencia)
+
+
+
+
+#Día 4
