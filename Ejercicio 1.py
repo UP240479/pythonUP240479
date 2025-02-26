cb3ac2f58302b00142c582c2d9d23471a0f78d8b
UP240479 <up240479@alumnos.upa.edu.mx>
2025-02-26 10:39:56 -0600

    26 de febrero

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -5,12 +5,14 @@ altura = float(input("ingrese el valor de la altura:"))
 area = 0.5 * base * altura
 print("El area del triangulo es:",area)
 
+
 #Perimetro de un triangulo
 lado = float(input("Ingrese el primer valor:"))
 lado2 = float(input("Ingrese el segundo valor:"))
 lado3 = float(input("Ingrese el tercer valor"))
 per = lado + lado2 + lado3 
 print("El perimetro del triangulo es:", per)
+
 
 #Calculos de un rectangulo
 largo = float(input("Ingresa el largo del rectangulo:"))
@@ -20,6 +22,7 @@ per = 2*(largo + ancho)
 print("El perimetro de tu rectangulo es:", per)
 print("El area de tu rectangulo es:", area)
 
+
 #Area de un circulo 
 pi = 3.1416
 radio = float(input("Ingresa el radio del circulo:"))
@@ -27,6 +30,7 @@ area = pi * radio **2
 print("El area de tu circulo es:", area)
 circunferencia = 2 * pi * radio 
 print("La circunferencia de tu circulo es:", circunferencia)
+
 
 #Calcula la Pendiente
 m = 2 
@@ -37,6 +41,7 @@ print("la Pendiente es:", m)
 print("La Interseccion con el eje y es:", intersecciony)
 print("La Interseccion con el eje x es:", interseccionx)
 
+
 #Distancia euclidiana
 import math 
 x1, y1 = 2, 2
@@ -45,6 +50,7 @@ m = (y2 - y1)/(x2 - x1)
 distancia = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 print("La Pendiente es:", m)
 print("La Distancia Euclidiana es:", distancia)
+
 
 #Comparar ambas pendientes
 m1 = (y2 - y1) / (x2 - x1)
@@ -60,16 +66,46 @@ print("Pendiente 2:", m2)
 print("Distancia euclidiana:", distancia)
 print(comparacion)
 
+
 #Calcular valor de y
 import math
-def calcular_y(x):
-    return x**2 + 6*x + 9
-ejemplox = [-5, -3, 0, 2, 5]
-print("Valores de Y para diferentes X:")
-for x in ejemplox:
-    print("Para x:{x}, y={calcular_y(x)}")
-a, b, c = 1, 6, 9
-x_raiz1 = (-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
-x_raiz2 = (-b - math.sqrt(b**2 - 4*a*c)) / (2*a)
-print("El valor de x donde y=0 es: X={x_raiz1} y x={x_raiz2}")
+num = float(input("Ingresa un numero:"))
+valory = num**2 +6*num + 9
+if num<0:
 
+#(Lo hago en mi casa)
+
+
+#Comparacion de palabras
+palabra1 = "Python"
+palabra2 = "Dragon"
+longitud1 = len(palabra1)
+longitud2 = len(palabra2)
+print("La longitud de Python es:", longitud1)
+print("La longitud de Dragon es:", longitud2)
+if longitud1 > longitud2:
+    print("La palabra Python es mas larga que Dragon.")
+else: 
+    print("La palabra Python no es mas larga que Dragon.")
+
+
+#Operadora and
+verificacion = "on" in palabra1 and "on" in palabra2
+print("on esta en Python y en Dragon:", verificacion)
+
+
+#Jergon 
+oracion = "Espero que este curso no este lleno de jerga"
+#contienejerga = jerga in oracion 
+#print("¿La oracion contiene jerga?", contienejerga) 
+if "jerga" in oracion:
+    print("Si se encuentra")
+else: 
+    print("No contiene jerga")
+
+
+#On en Python y Dragon
+if "on" in palabra1 and "on" in palabra2:
+    print("Si, on esta presente tanto en Python como en Dragon.")
+else:
+    print("No, no esta presente en ambas palabras.")
