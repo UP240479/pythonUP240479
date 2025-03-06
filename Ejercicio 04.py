4865291036bcb3b0bf99f6dbc7aa0d42ef7d64ed
UP240479 <up240479@alumnos.upa.edu.mx>
2025-03-05 21:19:01 -0600

    Martes y miercoles avance

---
+++ Ejercicio 04.py
@@ -0,0 +1,43 @@
+#Concatenarpalabras (1)
+Primera_palabra  = "Treinta"
+Segunda_palabra = "dias"
+Tercera_palabra = "de"
+Cuarta_palabra = "Python"
+Espacio = " "
+FraseCompleta = Primera_palabra + Espacio + Segunda_palabra + Espacio + Tercera_palabra + Espacio + Cuarta_palabra
+print (FraseCompleta)
+
+#Concatenar una cadena (2)
+PrimeraPalabra = "Codificacion"
+SegundaPalabra = "para"
+TerceraPalabra = "todos"
+Frase = PrimeraPalabra + Espacio + SegundaPalabra + Espacio + TerceraPalabra
+print(Frase)
+
+#empresa (3)
+Empresa = Frase
+
+#empresa (4)
+print (Empresa)
+
+#empresa (5)
+print(len(Empresa))
+
+#empresa(6)
+print(Empresa.upper())
+
+#empresa (7)
+print(Empresa.lower)
+
+#metodo capitalizar (8)
+print(Frase.capitalize())
+print(Frase.title())
+print(Frase.swapcase())
+
+#Recortar (9)
+Destajada = Empresa[12:]
+print(Destajada)
+
+#cadena codificacion (10)
+print(Subcadena.find("Codificacion")) 
+
