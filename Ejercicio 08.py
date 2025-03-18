3032df1a252ac996fbe308b056d14969c2a43c63
UP240479 <up240479@alumnos.upa.edu.mx>
2025-03-18 09:36:31 -0600

    .

---
+++ Ejercicio 08.py
@@ -0,0 +1,56 @@
+#INICIO DIA 8
+
+#Crea un diccionario vacío llamado perro
+PerroVacio = {}
+
+#Añade nombre, color, raza, patas y edad al diccionario de perros.
+Perro = {"Nombre":"Kichi", 
+   "Edad":"12 años", 
+    "Raza":"Pitbull", 
+   "Patas":"4"}
+print(Perro)
+
+#Cree un diccionario de estudiantes y agregue nombre, apellido, genero, edad, estado civil, habilidades, pais, cuidad y direccion como claves en el diccionario
+Estudiantes = {
+    "Nombre":"Christian",
+    "Apellidos":"Morales Vazquez",
+    "Genero":"Hombre",
+    "Edad":"18a años",
+    "Estado civil":"Soltero",
+    "Habilidades":"Bueno en basketball",
+    "Pais":"Mexico",
+    "Cuidad":"Aguascalienmtes",
+    "Direccion":{
+        "Calle":"Antonio Hernandez Gallegos",
+        "CP":"20206",
+    }
+}
+print(Estudiantes)
+
+#Obtenga la longitud del diccionario del estudiante
+print(len(Estudiantes))
+
+#Obtenga el valor de las habilidades y verifique el tipo de dato, debe ser una lista
+print(Estudiantes["Habilidades"])
+
+#Modifique los valores de las habilidades agregando una o dos habilidades
+Estudiantes["Habilidades"] = "Hiper foco"
+print(Estudiantes)
+
+#Obtenga las claves del diccionario como una lista
+Claves = Estudiantes.keys()
+print(Claves)
+
+#Obtener los valores del diccionario como una lista
+Valores = Estudiantes.values()
+print(Valores)
+
+#Cambie el diccionario a una lista de tuplas usando el método items()
+print(Estudiantes.items())
+
+#Eliminar uno de los elementos del diccionario
+Estudiantes.pop("Habilidades")
+print(Estudiantes)
+
+#Eliminar uno de los diccionarios
+del Perro
---
+++ Ejercicio 09.py
@@ -0,0 +1,12 @@
+#Obtener la información del usuario mediante input(“Ingrese su edad:”). Si el usuario tiene 18 años o más, indique: Tiene edad suficiente para conducir. Si es menor de 18 años, indique que espere los años que faltan. Resultado:
+Edad = float(input("Ingrese su edad: "))
+if Edad < 18:
+    print("No tienes edad suficiente para aprender a conducir")
+else:
+    print("Tienes la edad suficiente para aprender a conducir")
+
+#¿Quiene es mayor?
+TuEdad = float(input("Ingresa tu edad: "))
+MiEdad= 18
+
+#Obtener 2 numeros del usuario
